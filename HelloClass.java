5d2c70ff603f2e4186e0fecbd985ff39fa9144a9
F0murphy <89492817+F0murphy@users.noreply.github.com>
2021-08-28 20:11:58 -0400

    Add files via upload

---
+++ HelloClass.java
@@ -0,0 +1,17 @@
+/*
+Fangzheng Yu
+08.27.2021
+CSE 7: Lab 1
+introduce to myself
+*/
+public class HelloClass {
+    public static void main (String[] args) {
+        System.out.println("Fangzheng Yu");
+        System.out.println("This is my first year in Lehigh.");
+        System.out.println("1. I am from Chinese.");
+        System.out.println("2. I love history.");
+        System.out.println("3. I can play trembone.");                                                                                 
+        System.out.println("I hope that I could get all A in this year.");
+
+    }
+}
