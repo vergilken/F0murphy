2f5f3068ee591e599d3146d575e1af4901b5c09b
F0murphy <89492817+F0murphy@users.noreply.github.com>
2021-08-28 20:12:39 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Fangzheng Yu
+08.27.2021
+CSE2 Spring 2020: Lab 1
+Greet to the World
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        
+    }
+    
+}
